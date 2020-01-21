6488082ed0812753d341f6dcfef7a56c8f4543ef
askhatxx <55058367+askhatxx@users.noreply.github.com>
2020-01-22 00:31:49 +0600

    Second commit

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,41 +1,78 @@
 <template>
   <div class="calculator">
     <div class="display">
-      <div class="answer">Ans = -0.123</div>
-      <div class="current">-0.123</div>
+      <div class="answer">{{ answer ? `Ans = ${answer}` : `` }}</div>
+      <div class="current">{{ current }}</div>
     </div>
-    <button class="btn keys">←</button>
-    <button class="btn keys">AC</button>
-    <button class="btn keys">+/-</button>
+    <button @click='backspace' class="btn keys">←</button>
+    <button @click='allClear' class="btn keys">AC</button>
+    <button @click='addSign' class="btn keys">+/-</button>
     <button class="btn keys">Copy</button>
-    <button class="btn">7</button>
-    <button class="btn">8</button>
-    <button class="btn">9</button>
+    <button @click='addSymbol(`7`)' class="btn">7</button>
+    <button @click='addSymbol(`8`)' class="btn">8</button>
+    <button @click='addSymbol(`9`)' class="btn">9</button>
     <button class="btn operator">÷</button>
-    <button class="btn">4</button>
-    <button class="btn">5</button>
-    <button class="btn">6</button>
+    <button @click='addSymbol(`4`)' class="btn">4</button>
+    <button @click='addSymbol(`5`)' class="btn">5</button>
+    <button @click='addSymbol(`6`)' class="btn">6</button>
     <button class="btn operator">×</button>
-    <button class="btn">1</button>
-    <button class="btn">2</button>
-    <button class="btn">3</button>
+    <button @click='addSymbol(`1`)' class="btn">1</button>
+    <button @click='addSymbol(`2`)' class="btn">2</button>
+    <button @click='addSymbol(`3`)' class="btn">3</button>
     <button class="btn operator">−</button>
-    <button class="btn">0</button>
-    <button class="btn dot">.</button>
+    <button @click='addSymbol(`0`)' class="btn">0</button>
+    <button @click='addSymbol(`.`)' class="btn dot">.</button>
     <button class="btn equal">=</button>
     <button class="btn operator">+</button>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      answer: '-652',
+      current: '0',
+    }
+  },
+  computed: {
+    currentNumber() {
+      return Number(this.current);
+    }
+  },
+  methods: {
+    backspace() {
+      this.current = this.current.slice(0, -1);
+      if (this.current === '') this.current = '0';
+      if (this.current === '-') this.current = '-0';
+    },
+    allClear() {
+      this.answer = null;
+      this.current = '0';
+    },
+    addSign() {
+      this.current = this.current.charAt(0) === '-' ? this.current.slice(1) : `-${this.current}`;
+    },
+    addSymbol(symbol) {
+      if (symbol === '0' && (this.current === '0' || this.current === '-0')) return;
+      if (symbol === '.' && this.current.includes('.')) return;
+      if (this.current === '0' || this.current === '-0') {
+        if (symbol !== '.') {
+          this.current = `${this.current.slice(0, -1)}${symbol}`;
+          return;
+        }
+      }
+      this.current = `${this.current}${symbol}`;
+    }
+  }
+}
 </script>
 
 <style scoped>
 .calculator {
   background: #fff;
   width: 90%;
-  max-width: 350px;
+  max-width: 370px;
   border: 1px solid #dfe1e5;
   border-radius: 8px;
   display: grid;
@@ -59,8 +96,9 @@ export default {}
   margin-bottom: 4px;
 }
 .current {
-  font-size: 30px;
   color: #222;
+  font-size: 30px;
+  height: 34px;
   font-weight: normal;
   user-select: text;
 }
